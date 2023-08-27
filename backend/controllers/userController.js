be223bd1a431b9c1c67a13862055670c85b56299
Anthony-H <flohoa-a@outlook.com>
2023-08-27 22:23:09 +0200

    feat: add logout logic

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -64,7 +64,11 @@ export const registerUser = asyncHandler(async (req, res) => {
  * Route: POST /api/users/logout
  */
 export const logoutUser = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: 'Logout User' })
+    res.cookie('jwt', '', {
+        httpOnly: true,
+        expires: new Date(0),
+    })
+    res.status(200).json({ message: 'User logged out' })
 })
 
 /**
