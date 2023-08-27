6670e102cc2bf08181c3d9a6bae5b027864b3f8d
Anthony-H <flohoa-a@outlook.com>
2023-08-27 13:42:35 +0200

    feat: create and setup mongoDB connection

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 import dotenv from 'dotenv'
 import express from 'express'
+import { connectDB } from './config/db.js'
 import { errorHandler, notFound } from './middleware/errorMiddleware.js'
 import userRoutes from './routes/userRoutes.js'
 
 dotenv.config()
+connectDB()
 
 const port = process.env.PORT || 5000
 const app = express()
