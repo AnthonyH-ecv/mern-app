da21a98770574cebdc433ddbe1f79ebd2b332779
Anthony-H <flohoa-a@outlook.com>
2023-09-07 15:38:37 +0200

    chore: run prettier

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,10 @@
 import express from 'express'
 import {
-    authUser,
-    getUserProfile,
-    logoutUser,
-    registerUser,
-    updateUserProfile,
+  authUser,
+  getUserProfile,
+  logoutUser,
+  registerUser,
+  updateUserProfile,
 } from '../controllers/userController.js'
 import { protect } from '../middleware/authMiddleware.js'
 
@@ -14,8 +14,8 @@ router.post('/', registerUser)
 router.post('/auth', authUser)
 router.post('/logout', logoutUser)
 router
-    .route('/profile')
-    .get(protect, getUserProfile)
-    .put(protect, updateUserProfile)
+  .route('/profile')
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile)
 
 export default router
