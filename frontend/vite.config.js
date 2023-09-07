81dce6bdd496b956310253d956a495f636f6619d
Anthony-H <flohoa-a@outlook.com>
2023-09-07 16:38:17 +0200

    fix: remove server field in vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,13 +4,4 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-      },
-    },
-  },
 })
